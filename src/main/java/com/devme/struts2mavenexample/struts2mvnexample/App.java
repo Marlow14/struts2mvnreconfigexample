a800833d713c2d8dc46a14fddae720075cb82aea
Marlow14 <christophermarlow@ChristophersMBP.home>
2017-01-03 23:10:36 -0500

    First commit

---
+++ src/main/java/com/devme/struts2mavenexample/struts2mvnexample/App.java
@@ -0,0 +1,13 @@
+package com.devme.struts2mavenexample.struts2mvnexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
